82a3eb90895fc0deb903360b84cced5a0791147e
dior1101995 <66167675+dior1101995@users.noreply.github.com>
2026-05-05 18:46:56 +0700

    add memory reading module
    add memory reading module

--- memory_reader.py
+++ memory_reader.py
@@ -2,7 +2,8 @@ import random
 
 class MemoryReader:
     def read(self):
-        # Fake dữ liệu memory (giả lập)
+        #1 Fake dữ liệu memory (giả lập)
+        
         return {
             "hp": random.randint(20, 100),
             "mana": random.randint(10, 100)
